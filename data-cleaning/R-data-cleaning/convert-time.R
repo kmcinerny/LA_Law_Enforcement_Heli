8fda49cfcd2f3c4b4f9060c8cae881c4607e3b05
kmcinerny <kmcinerny@g.ucla.edu>
2022-05-04 00:54:20 -0700

    deleted repeat code

--- data-cleaning/R-data-cleaning/convert-time.R
+++ data-cleaning/R-data-cleaning/convert-time.R
@@ -10,20 +10,6 @@ library(plotly) #interactive graphics
 library(ggmap)
 
 
-
-## Oct 2019
-
-# load data 
-#pos10.19_df <- read_csv("/Users/katemcinerny/Documents/UCLA/Carceral_ecologies/heli_data/data/csv/10.19/pos10.19_df.csv")
-
-# convert timestamp to PST
-pos10.19_df$timestamp <- with_tz(ymd_hms(pos10.19_df$timestamp))
-
-write.csv(pos10.19_df, file= "/Users/katemcinerny/Documents/UCLA/Carceral_ecologies/heli_data/data/CSV/10.19/pos10.19_df.csv")
-
-
-
-
 ## May 2020
 
 # load data 
@@ -36,47 +22,6 @@ write.csv(pos5.20_df, file= "/Users/katemcinerny/Documents/UCLA/Carceral_ecologi
 
 
 
-
-
-
-
-
-
-
-
-
-
-# load data
-combo5.20_filt <- read_csv("/Users/katemcinerny/Documents/UCLA/Carceral_ecologies/heli_data/data/csv/combo5.20_filt.csv")
-#View(flights11.20_hood)
-
-
-# convert timestamp to PST
-combo5.20_filt$timestamp <- with_tz(ymd_hms(combo5.20_filt$timestamp))
-# convert created to PST
-x <- ymd_hms(combo5.20_filt$created)
-combo5.20_filt$created <- with_tz(x)
-# convert takeoff to PST
-x <- ymd_hms(combo5.20_filt$calculatedTakeoff)
-combo5.20_filt$takeoff <- with_tz(x)
-# convert landing to PST
-x <- ymd_hms(combo5.20_filt$calculatedLanding)
-combo5.20_filt$landing <- with_tz(x)
-
-
-write.csv(combo5.20_filt, file = "/Users/katemcinerny/Documents/UCLA/Carceral_ecologies/heli_data/data/CSV/combo5.20_PST.csv")
-
-
-
-# convert timestamp to PST Oct 2020
-#pos10.20_df$PSTtimestamp <- with_tz(ymd_hms(pos10.20_df$timestamp))
-
-#write.csv(pos10.20_df, file = "/Users/katemcinerny/Documents/UCLA/Carceral_ecologies/heli_data/data/CSV/pos10.20_df.csv")
-
-positions10.20 <- read_csv("/Users/katemcinerny/Documents/UCLA/Carceral_ecologies/heli_data/data/csv/positions10.20.csv")
-positions10.20$PSTtimestamp <- with_tz(ymd_hms(positions10.20$timestamp))
-
-write.csv(positions10.20, file= "/Users/katemcinerny/Documents/UCLA/Carceral_ecologies/heli_data/data/csv/positions10.20.csv")
 
 
 # separate date-time-hr
--- data-cleaning/R-data-cleaning/filter-fields.R
+++ data-cleaning/R-data-cleaning/filter-fields.R
@@ -10,10 +10,6 @@ library(plotly) #interactive graphics
 library(ggmap)
 
 
-flights6.20_filt <- subset(combined6.20_df, select= -c(aircraftModeS, callsign, depAirportIata, arrAirportIata, updated, source_flight, source_pos, squawkCode_flight, squawkCode_pos, scheduledDeparture, aircraftTypeDescription, estimatedArrival))
-write.csv(flights6.20_filt, file = "flights6.20_filt.csv")
-
-
 combo5.20_filt <- subset(combined5.20_df, select= -c(aircraftModeS, callsign, depAirportIata, arrAirportIata, updated, source_flight, source_pos, squawkCode_flight, squawkCode_pos, scheduledDeparture, aircraftTypeDescription, estimatedArrival))
 
 write.csv(combo5.20_filt, file = "/Users/katemcinerny/Documents/UCLA/Carceral_ecologies/heli_data/data/CSV/combo5.20_filt.csv")
