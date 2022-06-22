467ff38f4aedfde39820db526bfc3694426a15e1
kmcinerny <kmcinerny@g.ucla.edu>
2022-06-22 15:19:16 -0700

    updated tail number filter

--- data-cleaning/R-data-cleaning/add-flight-id-nullpos.R
+++ data-cleaning/R-data-cleaning/add-flight-id-nullpos.R
@@ -1,25 +1,24 @@
+rm(list = ls())
 library(tidyverse)
 library(jsonlite)
 
-
 # Read in the JSON file
-flight5.20_json <- read_json("/Users/katemcinerny/Documents/UCLA/Carceral_ecologies/heli_data/data/json/pretty-202005.json", simplifyVector = TRUE)
+flight10.19_json <- read_json("/Users/katemcinerny/Documents/UCLA/Carceral_ecologies/heli_data/data/json/pretty-202005.json", simplifyVector = TRUE)
 
 # Extract the flight data frame (one observation per flight)
 flight5.20_df <- flight5.20_json$flights %>% as_tibble() # making a data frame with just flights & nested positions
 
 # list tail numbers alphabetically
-q <- unique(flight5.20_df$aircraftRegistration)
-sort(q)
+sort(unique(flight5.20_df$aircraftRegistration))
 
 # filter to law enforcement
 
 flight5.20_df <- flight5.20_df %>% 
-  filter(aircraftRegistration %in% c("N108DJ", "N108PP","N130SK","N171WC","N213PF", "N221LA", "N223LA", 
-                                    "N224KB", "N225LA", "N228LA","N228LA", "N229LA","N239PS","N267LA",
+  filter(aircraftRegistration %in% c("N108DJ", "N108PP","N130SK","N171WC","N213PF", "N21844", "N221LA", "N223LA", 
+                                    "N224KB", "N225LA", "N226LA", "N228LA","N228LA", "N229LA","N239PS","N267LA", "N28LB",
                                     "N29LB", "N3202Q","N472LA", "N501RM", "N520PD", "N521PD", "N661PD",
-                                    "N662PD", "N664PD", "N665PD", "N668PD", "N80NT", "N8NF", "N913WB",
-                                    "N930DK", "N950SG", "N951LA", "N951LB", "N952JH", "N953LA", "N954LA",
+                                    "N662PD", "N664PD", "N665PD", "N668PD", "N80NT", "N8NF", "N854RJ", "N913WB",
+                                    "N930DK", "N950LA", "N950SG", "N951LA", "N951LB", "N952JH", "N953LA", "N954LA",
                                     "N955LA", "N956LA", "N957LA", "N958LA", "N959LA", "N960LA", "N961LA",
                                     "N961SD", "N962LA", "N963SD", "N966SD"))
 
@@ -60,6 +59,6 @@ pos5.20_df <- full_join(flight5.20_num, pos5.20_df, by = "flight_id")
 
 
 
-write.csv(pos5.20_df, file = "/Users/katemcinerny/Documents/UCLA/Carceral_ecologies/heli_data/data/csv/pos5.20_df.csv")
+write.csv(pos5.20_df, file = "/Users/katemcinerny/Documents/UCLA/Carceral_ecologies/heli_data/data/csv/5.20/pos5.20_df.csv")
 
 
