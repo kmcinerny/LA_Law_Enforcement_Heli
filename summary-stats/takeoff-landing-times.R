ff1a520d9a1db1ff2d464168fa160f9290494611
kmcinerny <kmcinerny@g.ucla.edu>
2022-07-22 17:15:04 -0700

    added list of last timestamps

--- summary-stats/takeoff-landing-times.R
+++ summary-stats/takeoff-landing-times.R
@@ -27,3 +27,20 @@ gf_histogram(~hr, stat="count",data=first_timestamps)
 
 # export so I can map it to see most common heliport areas
 write.csv(first_timestamps, "/Users/katemcinerny/Documents/UCLA/Carceral_ecologies/heli_data/data/CSV/5.20/first_timestamps_may.csv")
+
+
+# most common landing
+last_timestamps <- monthly_data %>%
+  arrange(flight_id, timestamp) %>%
+  # grouping by flight id
+  group_by(flight_id) %>% 
+  # filter to call only last timestamp for each unique flight id
+  slice(tail(row_number(timestamp),1)) %>%
+  # isolate hour from timestamp
+  mutate(hr = paste(str_extract(timestamp,
+                                pattern = "(?<= )[[:digit:]]+"),
+                    "00",
+                    sep = ":"))
+# then run a histogram to see most popular takeoff times
+gf_histogram(~hr, stat="count",data=last_timestamps)
+
